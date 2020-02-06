48c9f7ba85693234db9c86b058c833cc70eb53cc
Nodirbek as a Teacher <d2151915@urhen.com>
2020-02-06 19:19:18 +0500

    Add Table View Flip Animation

---
+++ GitExample/MyCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyCell.swift
+//  GitExample
+//
+//  Created by Nodirbek Asqarov on 2/6/20.
+//  Copyright © 2020 Nodirbek Asqarov. All rights reserved.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -8,12 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView = UITableView(frame: self.view.frame)
+        self.view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "MyCell", bundle: nil), forCellReuseIdentifier: "MyCell")
+        
+        tableView.separatorStyle = .none
+        
+        
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath) as? MyCell else {return UITableViewCell()}
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.transform = CGAffineTransform(scaleX: 0.8, y: 0.8).rotated(by: .pi/18)
+        cell.alpha = 0.6
+        
+        let redView = cell.viewWithTag(3)!
+        UIView.transition(with: redView, duration: 0.5, options: .transitionFlipFromTop, animations: {
+            cell.transform = .identity
+            cell.alpha = 1
+        }, completion: nil)
+        
+//        UIView.animate(withDuration: 0.5, delay: 0.1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+//            cell.transform = .identity
+//            cell.alpha = 1
+//        }, completion: nil)
+        
+    }
+    
 
 
 }
