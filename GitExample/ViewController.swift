e31b068b618393a7d2ffdc5fa96902a4ee40e26f
Nodirbek as a Teacher <d2151915@urhen.com>
2020-02-06 18:57:41 +0500

    Initial Commit

---
+++ GitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitExample
+//
+//  Created by Nodirbek Asqarov on 2/6/20.
+//  Copyright © 2020 Nodirbek Asqarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
