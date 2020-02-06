d757eb14a62706acd19b0c9da71eef44a676a292
Muhammadyunus <uzbekstudent1997@gmail.com>
2020-02-06 20:35:08 +0500

    asdfadsfasdfasdfsadfasdfsdfdsf

---
+++ GitExample/TaqiyClassOrStruct.swift
@@ -0,0 +1,18 @@
+//
+//  TaqiyClassOrStruct.swift
+//  GitExample
+//
+//  Created by Mukhammadyunus on 2/6/20.
+//  Copyright © 2020 Nodirbek Asqarov. All rights reserved.
+//
+
+import UIKit
+
+struct DataModel {
+    
+}
+
+
+class Hello {
+    
+}
